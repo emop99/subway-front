3053c60ad0b147c605991459a472e0b5146cecc0
hee <emop99@naver.com>
2023-09-16 18:28:57 +0900

    약관동의 페이지 개발

--- component/join/agree.tsx
+++ component/join/agree.tsx
@@ -1,7 +1,56 @@
+'use client'
+
 import {HomePageAgreeText, MarketingAgreeText, PersonalAgreeText} from "@/component/join/agreeText";
-import React from "react";
+import React, {useRef, useState} from "react";
 
 export default function Agree() {
+    const [allChecked, setAllChecked] = useState(false);
+    const [homePageAgree, setHomePageAgree] = useState(false);
+    const [personalAgree, setPersonalAgree] = useState(false);
+    const [marketingAgree, setMarketingAgree] = useState(false);
+    const homePageAgreeRef = useRef<HTMLInputElement>(null);
+    const personalAgreeRef = useRef<HTMLInputElement>(null);
+
+    const onChangeAllAgree = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAllChecked(e.target.checked);
+        setHomePageAgree(e.target.checked);
+        setPersonalAgree(e.target.checked);
+        setMarketingAgree(e.target.checked);
+    }
+    const onChangeHomePageAgree = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.checked) {
+            setAllChecked(false);
+        }
+        setHomePageAgree(e.target.checked);
+    }
+    const onChangePersonalAgree = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.checked) {
+            setAllChecked(false);
+        }
+        setPersonalAgree(e.target.checked);
+    }
+    const onChangeMarketingAgree = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.checked) {
+            setAllChecked(false);
+        }
+        setMarketingAgree(e.target.checked);
+    }
+
+    const onClickNext = (e: React.MouseEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (!homePageAgree) {
+            alert('홈페이지 이용약관에 동의해주세요.');
+            homePageAgreeRef.current && homePageAgreeRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            return false;
+        }
+        if (!personalAgree) {
+            alert('개인정보 수집 및 이용에 동의해주세요.');
+            personalAgreeRef.current && personalAgreeRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            return false;
+        }
+        location.href = '/join/2??marketingAgree=' + marketingAgree;
+    }
+
     return <section id="agreementPage">
         <h3>회원가입</h3>
         <div className="step">
@@ -12,10 +61,10 @@ export default function Agree() {
             </ul>
         </div>
         <div className="inner">
-            <h2>STAP.1 약관동의</h2>
-            <form action="join.html" id="agreement">
+            <h2>STEP.1 약관동의</h2>
+            <form id="agreement">
                 <div className="allAgree">
-                    <input type="checkbox" id="allAgreeCb"/>
+                    <input type="checkbox" id="allAgreeCb" checked={allChecked} onChange={onChangeAllAgree} tabIndex={1}/>
                     <label htmlFor="allAgreeCb" >
                         <i className="fa-solid fa-check"></i>
                         <strong>약관 전체 동의</strong>
@@ -23,8 +72,8 @@ export default function Agree() {
                 </div>
                 <div className="agreeMain">
                     <div className="agree">
-                        <div className="agreeBox">
-                            <input type="checkbox" id="agreeCb1"/>
+                        <div className="agreeBox" ref={homePageAgreeRef}>
+                            <input type="checkbox" id="agreeCb1" checked={homePageAgree} onChange={onChangeHomePageAgree} tabIndex={2}/>
                             <label htmlFor="agreeCb1" >
                                 <i className="fa-solid fa-check"></i>
                                 <span>(필수)</span>
@@ -37,8 +86,8 @@ export default function Agree() {
                     </div>
 
                     <div className="agree">
-                        <div className="agreeBox">
-                            <input type="checkbox" id="agreeCb2"/>
+                        <div className="agreeBox" ref={personalAgreeRef}>
+                            <input type="checkbox" id="agreeCb2" checked={personalAgree} onChange={onChangePersonalAgree} tabIndex={3}/>
                             <label htmlFor="agreeCb2">
                                 <i className="fa-solid fa-check"></i>
                                 <span>(필수)</span>
@@ -52,7 +101,7 @@ export default function Agree() {
 
                     <div className="agree">
                         <div className="agreeBox">
-                            <input type="checkbox" id="agreeCb3"/>
+                            <input type="checkbox" id="agreeCb3" checked={marketingAgree} onChange={onChangeMarketingAgree} tabIndex={4}/>
                             <label htmlFor="agreeCb3" >
                                 <i className="fa-solid fa-check"></i>
                                 <span>(선택)</span>
@@ -62,34 +111,9 @@ export default function Agree() {
                         <div className="agreeText">
                             <MarketingAgreeText/>
                         </div>
-                        <div className="agreeSmsCheck">
-                            <ul>
-                                <li>
-                                    <input type="checkbox" id="smsCb1"/>
-                                    <label htmlFor="smsCb1" >
-                                        <i className="fa-solid fa-check"></i>
-                                        <p>SMS/LMS/MMS 수신 동의</p>
-                                    </label>
-                                </li>
-                                <li>
-                                    <input type="checkbox" id="smsCb2"/>
-                                    <label htmlFor="smsCb2" >
-                                        <i className="fa-solid fa-check"></i>
-                                        <p>E-mail 수신 동의</p>
-                                    </label>
-                                </li>
-                                <li>
-                                    <input type="checkbox" id="smsCb3"/>
-                                    <label htmlFor="smsCb3" >
-                                        <i className="fa-solid fa-check"></i>
-                                        <p>알림톡 수신 동의</p>
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
-                <input type="submit" id="agreeNext" value="다음"/>
+                <input type="submit" id="agreeNext" value="다음" onClick={onClickNext}/>
             </form>
         </div>
     </section>
